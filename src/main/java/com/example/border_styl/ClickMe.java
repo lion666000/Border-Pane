b51692a15f2fe688d5154afcf4f5b9049c8b1aed
Duk <hlajak06@gasos-ro.cz>
2025-09-05 09:33:46 +0200

    haf

--- src/main/java/com/example/border_styl/ClickMe.java
+++ src/main/java/com/example/border_styl/ClickMe.java
@@ -14,6 +14,8 @@ public class ClickMe extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+        root.setTop(tlacitko);
         tlacitko = new Button();
         tlacitko.setText("Click Me");
         BorderPane pane = new BorderPane();
@@ -22,6 +24,21 @@ public class ClickMe extends Application {
         Scene scene = new Scene(pane, 300, 200);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        tlacitko = new Button("Top");
+        pane.setTop(tlacitko);
+
+        tlacitko = new Button("Bottom");
+        pane.setBottom(tlacitko);
+
+        tlacitko = new Button("Center");
+        pane.setCenter(tlacitko);
+
+        tlacitko = new Button("Left");
+        pane.setLeft(tlacitko);
+
+        tlacitko = new Button("Right");
+        pane.setRight(tlacitko);
     }
 
     public static void main(String[] args) {
